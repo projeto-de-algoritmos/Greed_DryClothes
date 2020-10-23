319c176cb20398ee7f4a9b31484ffc9bfec038b3
Bruno Nunes <brunocmo.nunes@gmail.com>
2020-10-23 13:58:49 -0300

    Add greed template

---
+++ inc/greed.hpp
@@ -0,0 +1,20 @@
+#ifndef GREED_HPP
+#define GREED_HPP
+
+#include <iostream>
+
+class Greed {
+
+    int size;
+    int timeToDry;
+    bool dried;
+
+    Greed();
+    ~Greed();
+
+    void init();
+
+
+};
+
+#endif
